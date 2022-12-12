9b4bcffe9e0448a027c803ebfbb08b2f85b99517
CL76 <83503901+CL76@users.noreply.github.com>
2022-12-12 18:44:38 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import cv2
 from  PIL import Image, ImageEnhance
 
-image = Image.open(r'...\Insights_Bees_logo.png') #Brand logo image (optional)
+#image = Image.open(r'...\Insights_Bees_logo.png') #Brand logo image (optional)
 
 #Create two columns with different width
 col1, col2 = st.columns( [0.8, 0.2])
@@ -14,8 +14,8 @@ with col1:               # To display the header text using css style
     </style> """, unsafe_allow_html=True)
     st.markdown('<p class="font">Upload your photo here...</p>', unsafe_allow_html=True)
     
-with col2:               # To display brand logo
-    st.image(image,  width=150)
+#with col2:               # To display brand logo
+#    st.image(image,  width=150)
 
 #Add a header and expander in side bar
 st.sidebar.markdown('<p class="font">My First Photo Converter App</p>', unsafe_allow_html=True)
