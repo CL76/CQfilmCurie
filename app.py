f2c84a9f4e4ca58c1a5d05cf74b6ecdeab35071b
CL76 <83503901+CL76@users.noreply.github.com>
2022-12-24 15:42:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -35,23 +35,7 @@ if uploaded_file is not None:
         st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
         st.image(image,width=300)  
 
-    with col2:
-        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
-        
-uploaded_file = st.file_uploader("", type=['jpg','png','jpeg'])
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    
-    col1, col2 = st.columns( [0.5, 0.5])
-    with col1:
-        st.markdown('<p style="text-align: center;">Before</p>',unsafe_allow_html=True)
-        st.image(image,width=300)  
 
-    with col2:
-        st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
-        filter = st.sidebar.radio('Covert your photo to:', ['Original','Black and White', 'Pencil Sketch', 'Blur Effect']) #Add the filter in the sidebar
-        
-#Add conditional statements to take the user input values
     with col2:
         st.markdown('<p style="text-align: center;">After</p>',unsafe_allow_html=True)
         filter = st.sidebar.radio('Covert your photo to:', ['Original','Gray Image','Black and White', 'Pencil Sketch', 'Blur Effect'])
